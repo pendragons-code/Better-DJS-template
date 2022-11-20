4482832fae47062733d72f93f9fdea5534132ebc
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-20 20:56:09 +0800

    Add files via upload

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,5 +1,5 @@
 module.exports = async (Jasbot, messageCreate) =>{
-	const { prefix } = require("../../config.json")
+	const { prefix, botid } = require("../../config.json")
 	if(messageCreate.author.bot || messageCreate.channel.type == "dm") return
 	let mainprefix = messageCreate.content.includes(prefix) ? prefix : `<@${botid}>`
 	if(messageCreate.content.indexOf(prefix) !==0) return
@@ -9,14 +9,14 @@ module.exports = async (Jasbot, messageCreate) =>{
 	if(!cmd) return
 	let maxargs = cmd.maxargs
 	let minperms = cmd.minperms
-	if(maxargs) for(let i = 0; i < maxargs; i  ++) if(args[i+1]) return messageCreate.channel.send("Too many arguments were provided!!!!")
-	if(minperms) for(let i = 0; i < minperms.length; i ++) if(!messageCreate.member.permissions.has(minperms[i])) return messageCreate.channel.send(`You are missing permissions! Come back when you have ${minperms[i].slice(26)}`)
+	if(maxargs) for(let i = 0; i < maxargs; i++) if(args[i+1]) return messageCreate.channel.send("Too many arguments were provided!!!!")
+	if(minperms) for(let i = 0; i < minperms.length; i++)if(messageCreate.member.permissions.has(minperms[i])) return messageCreate.channel.send(`You are missing permissions! Come back when you have ${Permissionbyname}`)
 	let tokensec = Jasbot.structures.get("tokensec")
 	tokensec.execute(Jasbot, messageCreate, args, mainprefix)
-	cmd.execute(Jasbot, messageCreate, args, mainprefix)
-	console.log(`${messageCreate.author.username} ran ${cmd.name} in ${messageCreate.guild.id}`).catch(() => {
-		messageCreate.channel.send("Something went wrong! Try again later!")
+	cmd.execute(Jasbot, messageCreate, args, mainprefix).catch(() => {
+		console.error()
 		console.log(messageCreate.content)
-		return console.error()
+		return messageCreate.channel.send("Something went wrong!")
 	})
+	console.log(`${messageCreate.author.username} ran ${cmd.name} in ${messageCreate.guild.id}`)
 }
