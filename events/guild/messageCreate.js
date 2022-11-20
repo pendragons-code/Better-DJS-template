26892142cd3e2fad0970d536cd3137bedf142403
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-21 00:12:27 +0800

    Add files via upload

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -1,6 +1,5 @@
 module.exports = async (Jasbot, messageCreate) =>{
 	const { prefix, botid } = require("../../config.json")
-	const { PermissionsBitField } = require("discord.js")
 	if(messageCreate.author.bot || messageCreate.channel.type == "dm") return
 	let mainprefix = messageCreate.content.includes(prefix) ? prefix : `<@${botid}>`
 	if(messageCreate.content.indexOf(prefix) !==0) return
@@ -9,9 +8,15 @@ module.exports = async (Jasbot, messageCreate) =>{
 	const cmd = Jasbot.commands.get(command) || Jasbot.command.find(cmd => cmd.aliases && cmd.aliases.includes(command))
 	if(!cmd) return
 	let maxargs = cmd.maxargs
+	if(maxargs) for(let i = 0; i < maxargs; i  ++) if(args[i+1]) return messageCreate.channel.send("Too many args!")
 	let minperms = cmd.minperms
-	if(maxargs) for(let i = 0; i < maxargs; i++) if(args[i+1]) return messageCreate.channel.send("Too many arguments were provided!!!!")
-	if(minperms) for(let i = 0; i < minperms.length; i++)if(messageCreate.member.permissions.has(PermissionsBitField.Flags[minperms[i]])) return messageCreate.channel.send(`You are missing permissions! Come back when you have \`${minperms[i]}\``)
+	if(minperms) for(let i = 0; i < minperms.length; i++) if(messageCreate.member.permissions.has(minperms[i])){
+		const PermList = require("../../permissions.json")
+		let query = minperms[i]
+		if(Array.isArray(minperms[i])) query = minperms[i][0]
+
+		return messageCreate.channel.send(`You do not have the right permissions! \`${PermList[query]}\``)
+}
 	let tokensec = Jasbot.structures.get("tokensec")
 	tokensec.execute(Jasbot, messageCreate, args, mainprefix)
 	cmd.execute(Jasbot, messageCreate, args, mainprefix).catch(() => {
